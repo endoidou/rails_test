b7b9c2c8aafd752945764f62e7a520f16470192b
end.of.june3@gmail.com <jun@jun-mac.local>
2016-03-14 00:10:50 +0900

    controller & view test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = Hash.new 
+    @user[:name] = 'tanaka taro'
+    @user[:username] = 'ahyohyo'
+    @user[:location] = 'Tokyo'
+    @user[:about] = 'first user'
   end
 end
